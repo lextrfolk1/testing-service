5eb8630034444d520b58da0ad988b46b0c5ac7e9
lextrfolk1 <lextrfolk1@gmail.com>
2025-07-24 23:45:40 +0530

    swagger testing

---
+++ src/main/java/com/test/controller/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.test.controller;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${swagger.server-url:/proxy}")
+    private String swaggerServerUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(swaggerServerUrl)));
+    }
+
+
+}
